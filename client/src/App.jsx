87ba9a82688343678276299f5310f4a7f45baf4a
geoquimica-gomez <geoquimica.gomez@gmail.com>
2024-08-13 17:09:01 -0400

    comenznado hito 3

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+//Componentes importados
+import NavbarApp from './components/Navbar';
+import Footer from './components/Footer';
+
+// Vistas importadas
+import Home from './views/Home';
+import RegisterPage from './views/RegisterPage';
+import LoginPage from './views/LoginPage';
+
+function App() {
+  console.log('App render');
+  return (
+    <div className='appGrid'>
+      <header>
+        <NavbarApp />
+      </header>
+
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/register"
+            element={<RegisterPage />}
+          />
+          <Route
+            path="/login"
+            element={<LoginPage />}
+          />
+        </Routes>
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/views/Home.jsx
@@ -0,0 +1,41 @@
+import Header from '../components/Header';
+import CardPizza from '../components/CardPizza';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+            <Container className="mt-4">
+                <Row className="justify-content-center">
+                    <Col md={4} className="mb-4 d-flex">
+                        <CardPizza
+                            name="Napolitana"
+                            price={5950}
+                            ingredients={["Mozzarella", "Tomates", "Jamón", "Orégano"]}
+                            img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+                        />
+                    </Col>
+                    <Col md={4} className="mb-4 d-flex">
+                        <CardPizza
+                            name="Española"
+                            price={6950}
+                            ingredients={["Mozzarella", "Gorgonzola", "Parmesano", "Provolone"]}
+                            img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
+                        />
+                    </Col>
+                    <Col md={4} className="mb-4 d-flex">
+                        <CardPizza
+                            name="Pepperoni"
+                            price={6950}
+                            ingredients={["Mozzarella", "Pepperoni", "Orégano"]}
+                            img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
